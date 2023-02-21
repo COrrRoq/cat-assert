3d026c6d2b1e93459c69e0a76fe22b1e1ddfbd15
COrrRoq <catherine.orr@roq.co.uk>
2023-02-21 15:20:50 +0000

    First push with 3 new tests

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -3,6 +3,7 @@ package com.credersi.animals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+
 import org.junit.Test;
 
 public class CatTest {
@@ -12,4 +13,32 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	@Test
+	public void testCatDead() {
+		Cat cat = new Cat();
+		assert (cat.isAlive());
+		cat.runs();
+		cat.runs();
+		if(cat.howHungry() <= 100);
+		assertTrue(cat.isAlive() == false);
+	}	
+	
+	@Test
+	public void testCatEatsAndRunsOnce() {
+		Cat cat = new Cat();
+		assert (cat.isAlive());
+		cat.feeds();
+		cat.runs();
+		assertEquals(cat.howHungry(), 60);
+	}
+	
+	@Test
+	public void testCatEatsAndRunsLoop() {
+		Cat cat = new Cat();
+		assert (cat.isAlive());
+		cat.runs();
+		if (cat.howHungry() <= 50) {
+			cat.feeds();}
+	}
+		
 }
